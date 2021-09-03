113b162bea7bf7736534d56d40cbd9c6bb5d6b21
pamudaid <ar.pamuda@gmail.com>
2021-09-03 11:09:49 +0700

    menambahakan paragraf

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Git Remote 2</title>
 </head>
+
 <body>
     <h1>Git Remote 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tempore fugit sint soluta eveniet excepturi odit sit iste. Eveniet vitae expedita reprehenderit dolorum laudantium ab provident similique nemo eius? Hic!</p>
 </body>
+
 </html>
